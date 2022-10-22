725fbcd0b515d4812fd4842ab5d7adab0a97d82f
lahiruchalana <lahiruchalanairesh@gmail.com>
2022-10-22 20:30:02 +0530

    distinct for avoid data duplication

---
+++ Distinct_Query.sql
@@ -0,0 +1,11 @@
+USE nyle;
+
+SELECT name FROM task;
+
+SELECT DISTINCT * FROM task;
+
+SELECT DISTINCT name FROM task;
+
+SELECT name, complation_date FROM task;
+
+SELECT DISTINCT name, complation_date FROM task;
