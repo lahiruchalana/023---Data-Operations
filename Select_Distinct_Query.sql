9706fcf9b24575535a7e14de419b760ef3f9b53e
lahiruchalana <lahiruchalanairesh@gmail.com>
2022-10-23 23:54:04 +0530

    distinct ones

---
+++ Select_Distinct_Query.sql
@@ -0,0 +1,11 @@
+USE nyle;
+
+SELECT name FROM task;
+
+SELECT DISTINCT name FROM task;
+
+SELECT COUNT(DISTINCT name) FROM task;
+
+SELECT COUNT(DISTINCT name) AS count_of_name FROM task;
+
+SELECT COUNT(*) AS count FROM(SELECT DISTINCT name FROM task);
