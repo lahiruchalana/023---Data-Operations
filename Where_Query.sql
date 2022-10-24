17fdf38400474f1c1ff7fc1fa0d583f44f20f9b6
lahiruchalana <lahiruchalanairesh@gmail.com>
2022-10-24 23:55:36 +0530

    advanced where with IN, LIKE, BETWEEN

--- Where_Query.sql
+++ Where_Query.sql
@@ -29,3 +29,20 @@ WHERE id=1;
 
 SELECT * FROM task 
 WHERE complation_date="2022-11-01";
+
+SELECT * FROM task
+WHERE id BETWEEN 2 AND 5;
+
+SELECT * FROM task
+WHERE name LIKE "%OOP%";
+
+SELECT * FROm task
+WHERE name LIKE "%OOP";
+
+SELECT * FROM task
+WHERE name IN ("Assignment of OOP", "Maths Class");
+
+SELECT * FROM task;
+
+SELECT * FROM task 
+WHERE name IN ("Assignment of OOP", "Maths class", "Python Class");
